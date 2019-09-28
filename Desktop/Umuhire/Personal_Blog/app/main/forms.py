a24f4f4758f66ad1bc5637f2b7a774aade03085e
UmuhireAnuarithe <anuarithemuhire@gmail.com.com>
2019-09-28 18:12:42 +0200

    fixed errors

--- Desktop/Umuhire/Personal_Blog/app/main/forms.py
+++ Desktop/Umuhire/Personal_Blog/app/main/forms.py
@@ -9,15 +9,15 @@ from ..models import Subscription
 class BlogForm(FlaskForm):
     title = StringField('Title', validators = [Required()])
     text = TextAreaField('Blog',validators = [Required()])
-    submit = SubmitField('Post')
+    submit = SubmitField('Submit')
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you', validators = [Required()])
     submit = SubmitField('Submit')
 
 class CommentForm(FlaskForm):
-    text = TextAreaField('Leave a Comment',validators = [Required()])
-    submit = SubmitField('Add Comment')
+    text = TextAreaField('Add a Comment',validators = [Required()])
+    submit = SubmitField('Submit Comment')
 
 class SubscribeForm(FlaskForm):
     email = StringField('Email address', validators=[Required(), Email()])
@@ -26,4 +26,4 @@ class SubscribeForm(FlaskForm):
     def validate_email(self, email):
         email = Subscription.query.filter_by(email=email.data).first()
         if email:
-            raise ValidationError('That email is already subscribed to our emailing list.')
+            raise ValidationError('That email is already subscribed .')
